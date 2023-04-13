cc0263980ef34ae1f8297e9da4f17caf0611367c
Leo <111384016+hashketchum208@users.noreply.github.com>
2023-04-13 13:48:42 -0600

    added schemas

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Post } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    users: async () => {
+      return User.find().populate('posts');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).populate('posts');
+    },
+    posts: async (parent, { postText }) => {
+      const params = postText ? { postText } : {};
+      return Post.find(params).sort({ createdAt: -1 });
+    },
+    post: async (parent, { postId }) => {
+      return Post.findOne({ _id: postId });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addPost: async (parent, { postText, postAuthor }) => {
+      const post = await Post.create({ postText, postAuthor });
+
+      await User.findOneAndUpdate(
+        { username: postAuthor },
+        { $addToSet: { posts: post._id } }
+      );
+
+      return post;
+    },
+    
+    removePost: async (parent, { postId }) => {
+      return Post.findOneAndDelete({ _id: postId });
+    },
+    
+  },
+};
+
+module.exports = resolvers;
